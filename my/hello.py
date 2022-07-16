dabb9d7d8acfd49cb989a09aed1df2a7e0fd9f37
RedPeanut <zamong99@gmail.com>
2022-07-16 22:19:36 +0900

    테스트 파일 업데이트

--- my/hello.py
+++ my/hello.py
@@ -5,9 +5,19 @@
 
 
 def main():
-    last_day_of_month = [31,28,31,30,31,30,31,31,30,31,30,31]
-    print(last_day_of_month[1])
-    print(last_day_of_month[2])
+
+    import os
+    import pandas as pd
+    import numpy as np
+
+    path = os.path.realpath(os.path.abspath(__file__))
+    curr_dir = os.path.dirname(path)
+    filepath = curr_dir + '/download/22_1Q_220716_011520.xlsx'
+    #filepath = curr_dir + '/download/22_2Q_220716_011758.xlsx'
+    data = pd.read_excel(filepath)
+
+    data = data.loc[data['PER'] > 1 and data['PBR'] > 0.3]
+
     pass
 
 
