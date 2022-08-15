bb7d678f85cacbeae19f4d633ffb7cc1879d9d53
RedPeanut <zamong99@gmail.com>
2022-08-15 10:22:38 +0900

    코드정리

--- my/tests/test.py
+++ my/tests/test.py
@@ -1,10 +1,10 @@
-from unittest import TestCase, main
+import unittest
 
 
-class MyTests(TestCase):
+class MyTests(unittest.TestCase):
     def test_something(self):
         self.assertEqual(1, 1)
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
